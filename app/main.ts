7e448b2e7e8169ac81062d907348d8f548884307
Felippe Nacif <felippenacif@gmail.com>
2024-06-03 21:52:43 -0300

    main: listen to connections

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,8 @@ const server = net.createServer((socket) => {
     socket.end();
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Starting HTTP server");
 
-// Uncomment this to pass the first stage
-// server.listen(4221, 'localhost', () => {
-//     console.log('Server is running on port 4221');
-// });
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
+});
